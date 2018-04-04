8ecb386bf84dcb8321d9bc31a4edfd84fdc60b65
Maciej Borkowski <maciekbb98@gmail.com>
2018-04-04 12:51:58 +0200

    Added Task class and TaskState enum

---
+++ src/main/java/me/drefos/model/Task.java
@@ -0,0 +1,62 @@
+package me.drefos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String description;
+    @NotNull
+    TaskState state;
+
+    public Task() {
+    }
+
+    public Task(@NotNull String title, @NotNull String description, @NotNull TaskState state) {
+        this.title = title;
+        this.description = description;
+        this.state = state;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskState getState() {
+        return state;
+    }
+
+    public void setState(TaskState state) {
+        this.state = state;
+    }
+}
