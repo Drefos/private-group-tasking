d89552658ac74e3489da113135043154f72b9a19
Maciej Borkowski <maciekbb98@gmail.com>
2018-04-10 09:05:50 +0200

    Fixed login issue with password encoder

--- src/main/java/me/drefos/security/SecurityConfig.java
+++ src/main/java/me/drefos/security/SecurityConfig.java
@@ -5,9 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
 
     @Bean
     public UserDetailsService customUserDetailsService() {
